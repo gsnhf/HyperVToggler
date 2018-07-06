a19d29a337b5ec3c45673418f9b00cc2efe84f7a
Florian Geisenhof <florian.geisenhof@gmail.com>
2018-07-06 06:16:26 +0200

    Update script for a better UX

--- HyperVToggler.ps1
+++ HyperVToggler.ps1
@@ -1,7 +1,32 @@
-if((Get-WindowsOptionalFeature -Online -FeatureName Microsoft-Hyper-V-All).State -eq "Enabled") {
-	Disable-WindowsOptionalFeature -Online -FeatureName Microsoft-Hyper-V-All
-} else {
-	Enable-WindowsOptionalFeature -Online -FeatureName Microsoft-Hyper-V-All
+$state = (Get-WindowsOptionalFeature -Online -FeatureName Microsoft-Hyper-V-All).State
+
+Write-Host Hyper-V state is $state
+
+if ($state -eq "Enabled") {
+    Write-Host Disable Hyper-V?
+}
+else {
+    Write-Host Enable Hyper-V?
 }
 
-Get-WindowsOptionalFeature -Online -FeatureName Microsoft-Hyper-V-All
+$message = 'Change Hyper-V state?'
+$question = 'Are you sure you want to proceed? Computer will be restarted.'
+
+$choices = New-Object Collections.ObjectModel.Collection[Management.Automation.Host.ChoiceDescription]
+$choices.Add((New-Object Management.Automation.Host.ChoiceDescription -ArgumentList '&Yes'))
+$choices.Add((New-Object Management.Automation.Host.ChoiceDescription -ArgumentList '&No'))
+
+$decision = $Host.UI.PromptForChoice($message, $question, $choices, 1)
+if ($decision -eq 0) {
+    if ($state -eq "Enabled") {
+		Write-Host 'Disable Hyper-V'
+		Disable-WindowsOptionalFeature -Online -FeatureName Microsoft-Hyper-V-All
+    }
+    else {
+		Write-Host 'Enable Hyper-V'
+		Enable-WindowsOptionalFeature -Online -FeatureName Microsoft-Hyper-V-All
+    }
+}
+else {
+    Write-Host 'Cancelled'
+}
